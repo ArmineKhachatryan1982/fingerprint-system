d2dfef7c6d3b63374e696f8ddf5f4bb8dbff78c8
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2025-04-21 12:33:13 +0400

    add domain name in validateCSRFToken

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,6 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'http://127.0.0.1:8000/*',
             'http://localhost:8000/*',
             "https://premium-entry-system.trigger.ltd/*",
+            "https://fingerprint-system.trigger.ltd/"
 
         ]);
 
